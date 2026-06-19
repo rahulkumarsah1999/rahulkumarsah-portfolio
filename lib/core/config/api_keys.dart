aa1a3121431190235aa071a77d9e791ce465fde9
rahulkumarsah1999 <rkhhp713@gmail.com>
2026-06-19 22:25:23 +0530

    Add api keys file for cloudflare build

---
+++ lib/core/config/api_keys.dart
@@ -0,0 +1,7 @@
+
+
+class ApiKeys {
+  static const String emailServiceId = 'service_9ols17n';
+  static const String emailTemplateId = 'template_b72ffta';
+  static const String emailPublicKey = '6cDDbhASJxgjxmQ0H';
+}
